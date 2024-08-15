01b35ec396233902bcffe87f6f32c099148956b4
Izabel Nascimento <izabelyale@hotmail.com>
2024-08-14 22:31:09 -0300

    refactoring agent code

--- ModelBasedAgent.py
+++ ModelBasedAgent.py
@@ -6,9 +6,7 @@ class ModelBasedAgent:
         self.state = {
             "obstacle_distance": float('inf'),
             "obstacle_height": 0,
-            "dino_y_position": 0,
-            "dino_is_jumping": False,
-            "time_since_last_jump": 0
+            "dino_is_jumping": False
         }
 
     def update_state(self, obstacles, dinosaur):
@@ -19,33 +17,29 @@ class ModelBasedAgent:
         else:
             self.state["obstacle_distance"] = float('inf')
 
-        self.state["dino_y_position"] = dinosaur.dino_rect.y
         self.state["dino_is_jumping"] = dinosaur.dino_jump
-        self.state["time_since_last_jump"] += 1
 
     def act(self, dinosaur, obstacles):
         action = self.decide_action(obstacles)
+        is_jumping = self.state["dino_is_jumping"]
 
-        if action == "jump" and not dinosaur.dino_jump:
+        if action == "jump" and not is_jumping:
             dinosaur.dino_duck = False
             dinosaur.dino_run = False
             dinosaur.dino_jump = True
-        elif action == "duck" and not dinosaur.dino_jump:
+        elif action == "duck" and not is_jumping:
             dinosaur.dino_duck = True
             dinosaur.dino_run = False
             dinosaur.dino_jump = False
 
     def decide_action(self, obstacles):
-        if self.state["obstacle_distance"] < 290:
-            if isinstance(obstacles[0], Bird):
-                if self.state["obstacle_height"] < 300:
-                    return "duck"
-                else:
-                    return "jump"
-            else:
-                return "jump"
-        else:
+        if self.state["obstacle_distance"] >= 290:
             return ""
+        obstacle = obstacles[0]
+        if isinstance(obstacle, Bird):
+            return "duck" if self.state["obstacle_height"] < 300 else "jump"
+
+        return "jump"
 
 
 model_based_agent = ModelBasedAgent()
--- SimpleReactiveAgent.py
+++ SimpleReactiveAgent.py
@@ -2,24 +2,25 @@ from Bird import Bird
 
 
 def simple_reactive_agent(obstacles, dinosaur):
-    if obstacles:
-        obstacle = obstacles[0]
-        distance_to_obstacle = obstacle.rect.x - dinosaur.dino_rect.x
+    if not obstacles:
+        return
 
-        if distance_to_obstacle < 290:
-            if isinstance(obstacle, Bird):
-                if obstacle.rect.y < 300:
-                    if not dinosaur.dino_jump:
-                        dinosaur.dino_duck = True
-                        dinosaur.dino_run = False
-                        dinosaur.dino_jump = False
-                else:
-                    if not dinosaur.dino_jump:
-                        dinosaur.dino_duck = False
-                        dinosaur.dino_run = False
-                        dinosaur.dino_jump = True
-            else:
-                if not dinosaur.dino_jump:
-                    dinosaur.dino_duck = False
-                    dinosaur.dino_run = False
-                    dinosaur.dino_jump = True
+    obstacle = obstacles[0]
+    distance_to_obstacle = obstacle.rect.x - dinosaur.dino_rect.x
+
+    if distance_to_obstacle >= 290 or dinosaur.dino_jump:
+        return
+
+    if isinstance(obstacle, Bird):
+        if obstacle.rect.y < 300:
+            dinosaur.dino_duck = True
+            dinosaur.dino_run = False
+            dinosaur.dino_jump = False
+        else:
+            dinosaur.dino_duck = False
+            dinosaur.dino_run = False
+            dinosaur.dino_jump = True
+    else:
+        dinosaur.dino_duck = False
+        dinosaur.dino_run = False
+        dinosaur.dino_jump = True
